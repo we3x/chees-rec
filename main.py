67a28063aeec1589d21a5a36464e3db03bd8c271
Veljko Simic <wex@tilda.center>
2016-10-19 16:20:21 +0200

    Template matching

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import collections
+import numpy as np
 import matplotlib.pyplot as plt
 
 def load_image(path):
@@ -16,6 +16,14 @@ def main():
     img_core = load_image("./images/initial.png")
     img_gray = image_gray(img_core)
     img_bin = image_bin(img_gray)
+    template = cv2.imread('./images/wp.png',0)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_bin,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.25
+    loc = np.where( res >= threshold)
+    print(loc)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_bin, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
     cv2.imwrite('nn.png',img_bin)
 
 
