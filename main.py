7763f4dbf435b6285f99ef2e0883615b16220165
wex <wex@tilda.center>
2016-11-19 15:46:31 +0100

    Insert in board

--- main.py
+++ main.py
@@ -15,9 +15,20 @@ def image_bin(image_gs):
     ret, image_bin = cv2.threshold(image_gs, 130, 255, cv2.THRESH_BINARY)
     return image_bin
 
-pieces = ['king', 'pawn', 'knight', 'bishop', 'queen', 'rook']
+pieces = ["king", "pawn", "knight", "bishop", "queen", "rook"]
 colors = ['black', 'white']
 data = []
+board = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+]
+
 
 def main():
     img_core = load_image("./images/initial.png")
@@ -27,14 +38,27 @@ def main():
     for color in colors:
         for piece in pieces:
             path = './images/'+color+'/'+piece+'.png'
-            label = color[0] + piece
-            component = cv2.imread(path)
-            component = cv2.resize(component, (30, 30))
+            label = piece
+            component_core = cv2.imread(path)
+            component_gs = image_gray(component_core)
+            component_bin = image_bin(component_gs)
+            component = cv2.resize(component_bin, (30, 30))
             vector = np.array(component).ravel()
             data.append({'label':label, 'sample':vector})
 
     EigenPieceChess = PCAModel()
     EigenPieceChess.train(data)
+    for i in range(8):
+        for j in range(8):
+            vector = np.array(img_bin[i*30:(i+1)*30, j*30:(j+1)*30]).ravel()
+            label, res = EigenPieceChess.classify(vector)
+            board[i][j] = pieces.index(label)
+            if label == "pawn":
+                if res > 1000:
+                    board[i][j] = 0
+
+    for i in board:
+        print(i)
 
 if __name__ == "__main__":
     main()
