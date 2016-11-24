92cb1a7163915f815e0eb02903355522cadc2a3d
wex <wex@tilda.center>
2016-11-24 09:39:42 +0100

    Remove comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from board import ChessBoard
 
 
 def main():
-    board = ChessBoard('./images/tests/5.png')
+    board = ChessBoard('./images/tests/1.png')
     for column in board.get_board():
         print(column)
 
